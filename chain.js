b53e86cd8b8edd4ce7bd245a33e81b9ae41ab2b9
YashWaghmare31 <75028726+YashWaghmare31@users.noreply.github.com>
2021-02-20 20:02:24 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,30 @@
+class chain{
+  constructor(bodyA,bodyB){
+    var options={
+        bodyA : bodyA,
+        bodyB : bodyB,
+        stiffness:.04,
+        length:10
+    }
+     this.chain=Constraint.create(options);
+     World.add(world,this.chain);
+}
+display() {
+    strokeWeight(4);
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+}
